4c2c235f555e2797f071e5bdfddb7e8acb394466
tiao1314 <1314tiao@gmail.com>
2023-02-16 19:22:00 +0000

    adding database to all pages

--- page-3.php
+++ page-3.php
@@ -1,18 +1,67 @@
 <?php
 require_once('database/database.php');
+
+// Get all games
+$query = 'SELECT * FROM games ORDER BY gameID';
+$statement = $db->prepare($query);
+$statement->execute();
+$games = $statement->fetchAll();
+$statement->closeCursor();
+
 ?>
 
 <?php
 include 'includes/include.php'
 ?>
 
+
 <main class="container">
-  <div class="starter-template text-center">
-    <h1>Page 3</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-  </div>
+<h1>Product List</h1>
+    <section>
+        <!-- display a table of game info -->
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Founded</th>
+                <th>genre</th>
+                <th>Platform</th>
+                <th>Age_rating</th>
+                <th>Price</th>
+                <th>Description</th>               
+            </tr>
+
+            <?php foreach ($games as $game) : ?>
+            <tr>
+                <td><?php echo $game['gameID']; ?></td>
+                <td><?php echo $game['name']; ?></td>
+                <td><?php echo $game['release_date']; ?></td>
+                <td><?php echo $game['genre']; ?></td>
+                <td><?php echo $game['platform']; ?></td>
+                <td><?php echo $game['age_rating']; ?></td>
+                <td><?php echo $game['price']; ?></td>
+                <td><?php echo $game['description']; ?></td>
+                
+                
+                
+
+                
+            </tr>
+            <?php endforeach; ?>
+        </table>
+
+
+            
+    </section>
+
+
 
 </main><!-- /.container -->
     <script src="js/bootstrap.bundle.min.js"></script>
+    <footer>
+    <p>&copy; <?php echo date("Y"); ?> Games Database, Inc.</p>
+</footer>
   </body>
 </html>
+
+
